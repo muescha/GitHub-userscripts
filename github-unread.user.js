e60b8206e0d5ec735a1a8f6a218497eab4831c46
Muescha <184316+muescha@users.noreply.github.com>
2020-06-06 04:13:44 +0200

    Create github-unread.user.js

---
+++ github-unread.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         GitHub: hide unread
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide unread issues in github issues list
+// @author       Muescha
+// @namespace    https://github.com/muescha
+// @match        https://github.com/*/*/issues
+// @match        https://github.com/*/*/issues?*
+// @match        https://github.com/*/*/pulls
+// @match        https://github.com/*/*/pulls?*
+// @grant        none
+// @updateURL   https://raw.githubusercontent.com/muescha/GitHub-userscripts/master/github-unread.user.js
+// @downloadURL https://raw.githubusercontent.com/muescha/GitHub-userscripts/master/github-unread.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getQueryParts(search) {
+		return search.split(/\s+/);
+	};
+
+    function includes(search, ...searchStrings) {
+		return getQueryParts(search).some(queryPart => searchStrings.includes(queryPart));
+	}
+
+    function init(){
+
+        var searchParams = new URLSearchParams(document.location.search);
+        var q = searchParams.get("q");
+
+        var isHideUnread = includes(q, "is:unread");
+        var isGreyUnread = includes(q, "is:unread-grey","is:unread-debug", "is:unreadr");
+
+
+        if(isGreyUnread || isHideUnread){
+
+            document.querySelectorAll(".js-navigation-container:first-child div.Box-row:not(.Box-row--unread)").forEach(el => {
+
+                if(isHideUnread){
+                    el.remove();
+                } else {
+                    let title = el.querySelector("a").innerText
+                    el.classList.add("Box--condensed");
+                    el.querySelector("input").outerHTML = `<div style="width:12px;"></div>`;
+                    var linkEl = el.querySelectorAll("div")[3]
+                    linkEl.innerHTML=title;
+                    linkEl.setAttribute("style","font-weight: 600!important;color:grey")
+                }
+            });
+        }
+    }
+
+    document.addEventListener("pjax:end", init);
+	init();
+
+})();
